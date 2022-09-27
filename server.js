8fe604df05f963621a03ea52414ae4744eeba44e
White Buffalo <apothocare@gmail.com>
2022-09-27 12:30:37 -0500

    Test get route successfully

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+const notes = require('./db/db.json');
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+
+});
