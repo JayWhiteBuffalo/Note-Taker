81c562943f8ab045aa8dba7df090a265abc77407
White Buffalo <apothocare@gmail.com>
2022-09-27 23:58:53 -0500

    Added create note function. Posting to database correctly.

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
+const fs = require('fs');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
 
-
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 const notes = require('./db/db.json');
 
+function createNote(body, noteArr){
+    console.log(body);
+    const note = body
+    noteArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(noteArr)
+    );
+    return noteArr;
+}
+
+//This will return notes in database in JSON format
 app.get('/api/notes', (req,res) => {
+    
     res.json(notes);
 });
+
+// Add Notes
+app.post('/api/notes', (req, res) => {
+    let note = createNote(req.body, notes);
+    console.log(note)
+    res.json(note);
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
